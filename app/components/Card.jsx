b814f5bdfb1842c53532744c10ab0a4a42569665
ElsayedRapaa <Elsayedrapaa7@gmail.com>
2023-03-29 17:00:24 +0200

    Add Series Components and Similar Components

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -2,25 +2,54 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export default function Card({ movie }) {
+export default function Card({ movie, series }) {
   return (
-    <Link
-      href={`/movie/${movie.id}`}
-      className="p-2 border dark:border-slate-700 rounded-lg hover:shadow-md drop-shadow-md dark:hover:shadow-slate-800"
-    >
-      <Image
-        src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-        alt="movie Poster"
-        width={200}
-        height={300}
-        quality={100}
-        style={{ maxWidth: "100%", maxHeight: "100%" }}
-        placeholder="blur"
-        blurDataURL="/rolling.svg"
-        className="rounded-t-md"
-      />
-      <p className="line-clamp-3 font-light text-sm pt-2">{movie.overview}</p>
-      <p className="truncate font-bold pt-2">{movie.title}</p>
-    </Link>
+    <>
+      {movie ? (
+        <Link
+          href={`/movie/${movie.id}`}
+          className="p-2 border dark:border-slate-700 rounded-lg hover:shadow-md drop-shadow-md dark:hover:shadow-slate-800"
+        >
+          <Image
+            src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+            alt="movie Poster"
+            width={200}
+            height={300}
+            quality={100}
+            style={{ maxWidth: "100%", maxHeight: "100%" }}
+            placeholder="blur"
+            blurDataURL="/rolling.svg"
+            className="rounded-t-md"
+          />
+          <p className="line-clamp-3 font-light text-sm pt-2">
+            {movie.overview}
+          </p>
+          <p className="truncate font-bold pt-2">{movie.title}</p>
+        </Link>
+      ) : (
+        <Link
+          href={`/series/${series.id}`}
+          className="p-2 border dark:border-slate-700 rounded-lg hover:shadow-md drop-shadow-md dark:hover:shadow-slate-800"
+        >
+          <Image
+            src={`https://image.tmdb.org/t/p/original/${series.poster_path}`}
+            alt="movie Poster"
+            width={200}
+            height={300}
+            quality={100}
+            style={{ maxWidth: "100%", maxHeight: "100%" }}
+            placeholder="blur"
+            blurDataURL="/rolling.svg"
+            className="rounded-t-md"
+          />
+          <p className="line-clamp-3 font-light text-sm pt-2">
+            {series.overview}
+          </p>
+          <p className="truncate font-bold pt-2">
+            {series?.title || series?.name}
+          </p>
+        </Link>
+      )}
+    </>
   );
 }
